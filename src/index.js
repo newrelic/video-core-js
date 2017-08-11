34c072f90b24d3ccc1c5cffbc348dc9ce4ff7459
Jordi Aguilar Gorina <jaguilargorina@newrelic.com>
2017-08-11 09:34:51 -0700

    doc index

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@ import Emitter from './emitter'
 import Tracker from './tracker'
 import TrackerState from './trackerstate'
 
+/**
+ * New Relic Video namespace.
+ */
 module.exports = {
+  /** @borrows nrvideo.Constants */
   Constants,
+  /** @borrows nrvideo.Log */
   Log,
+  /** @borrows nrvideo.Emitter */
   Emitter,
+  /** @borrows nrvideo.Tracker */
   Tracker,
+  /** @borrows nrvideo.TrackerState */
   TrackerState,
+  /** @borrows nrvideo.Core */
   Core
 }
